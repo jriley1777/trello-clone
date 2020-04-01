002bc6793d122d611a96c74ebdc55df610dd9aaf
Joseph Riley <jriley@Josephs-MacBook-Air.local>
2020-04-01 15:54:02 -0400

    add basic scaffold for needed routes and implement react-router

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AppRoutes from '../components/AppRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Trello clone</div>
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
